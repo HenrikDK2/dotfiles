07aab5a582ee8477621af5e289e95817cbbce32f
HenrikDK2 <henrik265@outlook.dk>
2022-06-08 16:57:16 +0200

    Reorder install script

--- .my-scripts/init/install.sh
+++ .my-scripts/init/install.sh
@@ -2,43 +2,6 @@
 
 name=$(whoami)
 
-# Add bootloader entries
-clear
-while true; do
-    echo ""
-    read -p "Only for systemd-boot! - Do you want to add TKG bootloader entry? [y/n] " yn
-    case $yn in
-        [Yy]* ) source ~/.my-scripts/init/bootloader.sh; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-# Ultrawide gaps on workspace 1
-clear
-while true; do
-    echo "Do you have a 5120x1440 ultrawide monitor,"
-    read -p "and do you want to have a 1440p window in the center on workspace 1? [y/n] " yn
-    if [[ "$yn" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-        if ! grep -Rq "workspace 1 gaps horizontal 1280" ~/.config/sway/config
-        then
-            sed '/^#Workspace 1 gaps$/r'<(
-                echo "workspace 1 gaps inner 0"
-                echo "workspace 1 gaps horizontal 1280"
-                echo "workspace 1 gaps top 0"
-            ) -i -- ~/.config/sway/config
-        fi
-        break;
-    elif [[ "$yn" =~ ^([nN])$ ]]; then
-        sed -i '/workspace 1 gaps inner 0/d' ~/.config/sway/config
-        sed -i '/workspace 1 gaps horizontal 1280/d' ~/.config/sway/config
-        sed -i '/workspace 1 gaps top 0/d' ~/.config/sway/config
-        break;
-    else
-       echo "Please answer yes or no."
-    fi
-done
-
 # Enable multilib pacman and ParallelDownloads
 multilibLine=$(grep -n "\[multilib\]" /etc/pacman.conf | cut -d":" -f1)
 let "multilibIncludeLine = $multilibLine + 1"
@@ -86,34 +49,6 @@ then
     done
 fi
 
-# Install building tools
-sudo pacman -S base-devel --noconfirm
-
-# Install yay
-cd /opt
-sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chmod 777 -R ./yay-git
-cd yay-git
-makepkg -si --noconfirm
-
-# Mesa-git
-clear
-while true; do
-    read -p "Do you have an AMD gpu? [y/n] " yn
-    case $yn in
-        [Yy]* ) yay -Syu mesa-git lib32-mesa-git --noconfirm; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-# General packages
-sudo pacman -S profile-sync-daemon --noconfirm
-yay -Syu gamemode lib32-gamemode vulkan-tools wireplumber cmst libpipewire02 qt5-wayland swaylock-fancy mako man-db swayidle xdg-desktop-portal gnome-keyring polkit-gnome seahorse libsecret xdg-desktop-portal-wlr glxinfo slurp sway deluge deluge-gtk swaybg xorg-xwayland bash-completion rofi sysmontask scrot micro pavucontrol nemo nemo-fileroller npm kitty firefox gvfs gvfs-mtp gvfs-gphoto2 code wl-clipboard unrar waybar unzip evolution evolution-ews pipewire pipewire-alsa pipewire-pulse irqbalance swappy grim --noconfirm
-
-# Fonts
-yay -Syu otf-font-awesome ttf-mac-fonts ttf-google-fonts-git ttf-ms-win11-auto ttf-ms-win11-auto-japanese ttf-ms-win11-auto-korean ttf-ms-win11-auto-other ttf-ms-win11-auto-sea ttf-ms-win11-auto-thai ttf-ms-win11-auto-zh_cn ttf-ms-win11-auto-zh_tw --noconfirm
-
 # If username isn't the same as Henrik, replace name in these files
 if [ "$name" != "henrik" ]; then
 	sudo sed -i "s/henrik/$name/g" /etc/sudoers
@@ -133,6 +68,86 @@ then
 	echo "password	optional	pam_gnome_keyring.so" | sudo tee -a /etc/pam.d/passwd
     clear
 fi
+
+# Install building tools
+sudo pacman -S base-devel --noconfirm
+
+# Install yay
+cd /opt
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chmod 777 -R ./yay-git
+cd yay-git
+makepkg -si --noconfirm
+
+# Add bootloader entries, and install kernel
+clear
+while true; do
+    echo ""
+    read -p "Only for systemd-boot! - Do you want to add TKG bootloader entry? [y/n] " yn
+    case $yn in
+        [Yy]* ) source ~/.my-scripts/init/bootloader.sh; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Ultrawide gaps on workspace 1
+clear
+while true; do
+    echo "Do you have a 5120x1440 ultrawide monitor,"
+    read -p "and do you want to have a 1440p window in the center on workspace 1? [y/n] " yn
+    if [[ "$yn" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        if ! grep -Rq "workspace 1 gaps horizontal 1280" ~/.config/sway/config
+        then
+            sed '/^#Workspace 1 gaps$/r'<(
+                echo "workspace 1 gaps inner 0"
+                echo "workspace 1 gaps horizontal 1280"
+                echo "workspace 1 gaps top 0"
+            ) -i -- ~/.config/sway/config
+        fi
+        break;
+    elif [[ "$yn" =~ ^([nN])$ ]]; then
+        sed -i '/workspace 1 gaps inner 0/d' ~/.config/sway/config
+        sed -i '/workspace 1 gaps horizontal 1280/d' ~/.config/sway/config
+        sed -i '/workspace 1 gaps top 0/d' ~/.config/sway/config
+        break;
+    else
+       echo "Please answer yes or no."
+    fi
+done
+
+# Mesa-git
+clear
+while true; do
+    read -p "Do you have an AMD gpu? [y/n] " yn
+    case $yn in
+        [Yy]* ) yay -Syu mesa-git lib32-mesa-git --noconfirm; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Sync browser to ram
+sudo pacman -S profile-sync-daemon --noconfirm
+sudo systemctl --user enable psd
+
+# Optimized Firefox profile
+clear
+while true; do
+    echo "Do you wish to use an optimized Firefox profile?"
+    read -p "This will reset your current profile? [y/n] " yn
+    case $yn in
+        [Yy]* ) rm -rf ~/.mozilla; cp -r ~/.my-scripts/init/.mozilla ~/.mozilla; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# General packages
+yay -Syu gamemode lib32-gamemode vulkan-tools wireplumber cmst libpipewire02 qt5-wayland swaylock-fancy mako man-db swayidle xdg-desktop-portal gnome-keyring polkit-gnome seahorse libsecret xdg-desktop-portal-wlr glxinfo slurp sway deluge deluge-gtk swaybg xorg-xwayland bash-completion rofi sysmontask scrot micro pavucontrol nemo nemo-fileroller npm kitty firefox gvfs gvfs-mtp gvfs-gphoto2 code wl-clipboard unrar waybar unzip evolution evolution-ews pipewire pipewire-alsa pipewire-pulse irqbalance swappy grim --noconfirm
+
+# Fonts
+yay -Syu otf-font-awesome ttf-mac-fonts ttf-google-fonts-git ttf-ms-win11-auto ttf-ms-win11-auto-japanese ttf-ms-win11-auto-korean ttf-ms-win11-auto-other ttf-ms-win11-auto-sea ttf-ms-win11-auto-thai ttf-ms-win11-auto-zh_cn ttf-ms-win11-auto-zh_tw --noconfirm
 
 # Theme
 gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
@@ -166,22 +181,6 @@ sudo chmod -x /usr/lib/evolution-data-server/evolution-alarm-notify
 
 # Other
 mkdir /home/$name/Screenshots
-
-# Optimized Firefox profile
-clear
-while true; do
-    echo "Do you wish to use an optimized Firefox profile?"
-    read -p "This will reset your current profile? [y/n] " yn
-    case $yn in
-        [Yy]* ) rm -rf ~/.mozilla; cp -r ~/.my-scripts/init/.mozilla ~/.mozilla; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-# Sync browser to ram
-sudo systemctl --user enable psd
-
 
 # Reboot
 clear
