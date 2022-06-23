0ed61c82fb87a3fa08c935fe045fa7dc9b50dba2
HenrikDK2 <henrik265@outlook.dk>
2022-06-23 17:35:16 +0200

    Adding vscode dotfiles + extensions

---
+++ .my-scripts/init/code-extensions.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install vscode plugins
+plugins=("stylelint.vscode-stylelint" "ritwickdey.LiveServer" "pkief.material-icon-theme" "formulahendry.code-runner" "jpoissonnier.vscode-styled-components" "formulahendry.auto-rename-tag" "formulahendry.auto-close-tag" "esbenp.prettier-vscode" "eamodio.gitlens" "dsznajder.es7-react-js-snippets" "dbaeumer.vscode-eslint" "bmalehorn.vscode-fish")
+
+for plugin in "${plugins[@]}"
+do
+    code --install-extension $plugin
+done
+
+clear
