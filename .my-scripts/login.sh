47ed286e48d303e5ab5fe79426f612bb4a9166a8
HenrikDK2 <henrik265@outlook.dk>
2022-07-24 21:54:14 +0200

    Fixes

--- .my-scripts/login.sh
+++ .my-scripts/login.sh
@@ -41,7 +41,7 @@ while true; do
     set_prio corectrl_helper 20 idle
     set_prio mako 20 idle
     set_prio polkit-gnome-authentication-agent-1 20 idle
-    set_prio Discord 10 idle
+    set_prio discord 10 idle
     set_prio steam 20 idle
     set_prio waybar 20 idle
     set_prio evolution 20 idle
