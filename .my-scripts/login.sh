b99ef3a2c61a289c456ac021e7f5af2bae323798
HenrikDK2 <henrik265@outlook.dk>
2022-07-24 23:34:00 +0200

    Seperate priorites for live adjustments

--- .my-scripts/login.sh
+++ .my-scripts/login.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-set_prio () {
-    pid=$(pidof $1)
-    if [ -n "$pid" ]; then
-        renice -n $2 -p $pid
-        if [ -n "$3" ]; then
-            ionice -c $3 -p $pid
-        fi
-    fi
-}
-
 launch () {
     uppercase=${1^}
     if [ -z "$(pidof $1)" ] && [ -z "$(pidof $uppercase)" ]; then
@@ -32,18 +22,5 @@ ionice -c idle -p $$
 
 # Priority of processes (name, niceness, ionice class)
 while true; do
-    set_prio corectrl 20 idle
-    set_prio corectrl_helper 20 idle
-    set_prio mako 20 idle
-    set_prio polkit-gnome-authentication-agent-1 20 idle
-    set_prio Discord 10 idle
-    set_prio steam 20 idle
-    set_prio steamwebhelper 20 idle
-    set_prio waybar 20 idle
-    set_prio evolution 20 idle
-    set_prio swayidle 20 idle
-    set_prio swaybg 20 idle
-    set_prio pipewire -18
-    set_prio pipewire-pulse -18
-    sleep 30
+    ~/.my-scripts/prio.sh
 done
