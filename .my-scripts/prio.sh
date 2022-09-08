8e23cb96020b4e7e611b0681145ad845b3661c75
HenrikDK2 <henrik265@outlook.dk>
2022-09-08 23:32:26 +0200

    New entry to mimeapps, and improved prio script

--- .my-scripts/prio.sh
+++ .my-scripts/prio.sh
@@ -1,5 +1,5 @@
 set_prio () {
-    pids=$(pgrep -f $1)
+    pids=$(pgrep -f "$1")
     if [ -n "$pids" ]; then
         renice -n $2 -p $pids
         if [ -n "$3" ]; then
@@ -8,20 +8,23 @@ set_prio () {
     fi
 }
 
-set_prio login.sh 20 idle
-set_prio corectrl 20 idle
-set_prio mako 20 idle
-set_prio polkit-gnome 20 idle
-set_prio discord 10 idle
-set_prio waybar 20 idle
-set_prio .local/share/Steam 10 idle
-set_prio Origin 10 idle
-set_prio evolution 20 idle
-set_prio swayidle 20 idle
-set_prio swaybg 20 idle
-set_prio pipewire -18
+# ps ax -o pid,ni,cmd
+# You can use the cmd output to match the specfic process.
+set_prio "login.sh" 20 idle
+set_prio "corectrl" 20 idle
+set_prio "mako" 20 idle
+set_prio "polkit-gnome" 20 idle
+set_prio "discord" 10 idle
+set_prio "waybar" 20 idle
+set_prio ".local/share/Steam" 10 idle
+set_prio "Origin" 10 idle
+set_prio "evolution" 20 idle
+set_prio "swayidle" 20 idle
+set_prio "swaybg" 20 idle
+set_prio "Rockstar Games Launcher" 20 idle
+set_prio "pipewire" -18
 
 # Steam Games - Same priority as gamemode
-set_prio steamapps -15 
+set_prio "steamapps" -15 
 
 sleep 30
