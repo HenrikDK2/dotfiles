5c6ddf03a2e97fee656891c15189e2b97bc847a6
HenrikDK2 <henrik265@outlook.dk>
2022-09-13 08:10:55 +0200

    Update

--- .my-scripts/prio.sh
+++ .my-scripts/prio.sh
@@ -10,21 +10,22 @@ set_prio () {
 
 # ps ax -o pid,ni,cmd
 # You can use the cmd output to match the specfic process.
-set_prio "login.sh" 20 idle
 set_prio "corectrl" 20 idle
 set_prio "mako" 20 idle
 set_prio "polkit-gnome" 20 idle
-set_prio "discord" 10 idle
 set_prio "waybar" 20 idle
-set_prio ".local/share/Steam" 10 idle
-set_prio "Origin" 10 idle
 set_prio "evolution" 20 idle
 set_prio "swayidle" 20 idle
 set_prio "swaybg" 20 idle
-set_prio "Rockstar Games Launcher" 20 idle
+set_prio "discord" 10 idle
+set_prio "steamwebhelper" 10 idle
+set_prio "Origin" 10 idle
+set_prio "/opt/Heroic/heroic --" 10 idle
+
 set_prio "pipewire" -18
 
-# Steam Games - Same priority as gamemode
-set_prio "steamapps" -15 
+# Games - Same priority as gamemode
+set_prio "steamapps" -15
+set_prio ".exe" -15 
 
 sleep 30
