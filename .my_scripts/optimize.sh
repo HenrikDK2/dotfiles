92483034b878c04858cd071c2157faab1b9e12c8
HenrikDK2 <henrik265@outlook.dk>
2022-09-21 00:54:50 +0200

    Improvements to optimize script

--- .my_scripts/optimize.sh
+++ .my_scripts/optimize.sh
@@ -1,18 +1,26 @@
+# Parameters
+# 1. You can use the cmd output to match the specfic process.
+# 2. Niceness values range from -20 to 20, lower is higher priority.
+# 3. Ionice class, 0 for none, 1 for real time, 2 for best effort, 3 for idle.
+# 4. Ionice classdata, range from 0-7, lower is higher. Idle class doesn't allow classdata, so it's ignored.
 set_prio () {
     pids=$(pgrep -f "$1")
     if [ -n "$pids" ]; then
-        renice -n $2 -p $pids
-        if [ -n "$3" ]; then
-            ionice -c $3 -p $pids
+        sudo renice -n $2 -p $pids
+        if ([ $3 ] && [ -z $4 ]) || [ $3 -eq 3 ]; then
+            sudo ionice -c $3 -p $pids
+        fi
+
+        if [ $3 -ne 3 ] && [ $4 ]; then
+            sudo ionice -c $3 -n $4 -p $pids
         fi
     fi
 }
 
 # Clear RAM
-sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'  
+sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
 
-# ps ax -o pid,ni,cmd
-# You can use the cmd output to match the specfic process.
+# Set priorities
 set_prio "corectrl" 20 3
 set_prio "mako" 20 3
 set_prio "polkit-gnome" 20 3
@@ -27,4 +35,5 @@ set_prio "/opt/Heroic/heroic --" 10 3
 
 # Games - Same priority as gamemode
 sleep 30
-set_prio "steamapps" -10 1
+set_prio "steamapps" -10 1 0
+set_prio "/*[.]exe" -10 1 0
