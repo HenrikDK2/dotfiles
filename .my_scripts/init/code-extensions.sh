2a5515fa5af3a6137de57d867b5fba459336c6c4
HenrikDK2 <henrik265@outlook.dk>
2022-11-03 17:38:32 +0100

    Removed a few plugins, and made array more readable

--- .my_scripts/init/code-extensions.sh
+++ .my_scripts/init/code-extensions.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
 # Install vscode plugins
-plugins=("redhat.vscode-yaml" "llvm-vs-code-extensions.vscode-clangd" "GraphQL.vscode-graphql" "wix.vscode-import-cost" "mikestead.dotenv" "stylelint.vscode-stylelint" "ritwickdey.LiveServer" "pkief.material-icon-theme" "formulahendry.code-runner" "jpoissonnier.vscode-styled-components" "formulahendry.auto-rename-tag" "formulahendry.auto-close-tag" "esbenp.prettier-vscode" "eamodio.gitlens" "dsznajder.es7-react-js-snippets" "dbaeumer.vscode-eslint" "bmalehorn.vscode-fish")
+plugins=(
+    "redhat.vscode-yaml"
+    "llvm-vs-code-extensions.vscode-clangd"
+    "mikestead.dotenv"
+    "stylelint.vscode-stylelint"
+    "ritwickdey.LiveServer"
+    "pkief.material-icon-theme"
+    "formulahendry.code-runner"
+    "jpoissonnier.vscode-styled-components"
+    "formulahendry.auto-rename-tag"
+    "formulahendry.auto-close-tag"
+    "esbenp.prettier-vscode"
+    "eamodio.gitlens"
+    "dbaeumer.vscode-eslint"
+    "bmalehorn.vscode-fish"
+    )
 
 for plugin in "${plugins[@]}"
 do
