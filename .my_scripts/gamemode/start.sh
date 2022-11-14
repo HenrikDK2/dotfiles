d14a6e49ecdc1550087db4910c7a241adac9f589
HenrikDK2 <henrik265@outlook.dk>
2022-11-14 01:11:43 +0100

    Lower priority of secondary wine processes that isn't related to game process

--- .my_scripts/gamemode/start.sh
+++ .my_scripts/gamemode/start.sh
@@ -10,9 +10,19 @@ done
 # Clear RAM
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
 
-sleep 30
+sleep 60
 
 # Games - Same priority as gamemode
 set_prio "steamapps/common/" -10 1 0
 set_prio "/*[.]exe" -10 1 0
 set_prio "obs" -10 1 0
+
+# Lower WINE proccesses
+set_prio "tabtip.exe" -5 2 0
+set_prio "wineserver" -5 2 0
+set_prio "explorer.exe" -5 2 0
+set_prio "plugplay.exe" -5 2 0
+set_prio "winedevice.exe" -5 2 0
+set_prio "services.exe" -5 2 0
+set_prio "start.exe" -5 2 0
+set_prio "rpcss.exe" -5 2 0
