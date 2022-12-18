fb563eaada9101d116f5ec3f431052139e70a453
HenrikDK2 <henrik265@outlook.dk>
2022-12-18 15:34:17 +0100

    Increase lines req in condition

--- .my_scripts/init/denyhosts/denyhosts.sh
+++ .my_scripts/init/denyhosts/denyhosts.sh
@@ -7,7 +7,7 @@ wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
 
 if [[ $? -eq 0 ]]; then
     wget --limit-rate=200k  -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts
-    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "100" ]; then
+    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "636000" ]; then
         sudo rm /etc/hosts.deny
         mv /etc/hosts.deny.new /etc/hosts.deny
         systemctl restart dnsmasq.service
