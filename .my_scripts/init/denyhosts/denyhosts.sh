3e14a071e2ef1b1eb038cba8c377288bbd3b160e
HenrikDK2 <henrik265@outlook.dk>
2022-12-18 16:44:38 +0100

    Combine StevenBlack hosts and ultimate blacklist hosts file

--- .my_scripts/init/denyhosts/denyhosts.sh
+++ .my_scripts/init/denyhosts/denyhosts.sh
@@ -6,13 +6,21 @@ ionice -c idle -p $$
 wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
 
 if [[ $? -eq 0 ]]; then
-    wget --limit-rate=200k  -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts
-    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "636000" ]; then
-        sudo rm /etc/hosts.deny
+    # Output and combine hosts files
+    wget --limit-rate=200k -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts
+    wget --limit-rate=200k -O - >> "/etc/hosts.deny.new" https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+   
+    # Remove duplicates
+    awk -i inplace '!seen[$0]++' /etc/hosts.deny.new
+
+    # Replace current hosts.deny with new one
+    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "750000" ]; then
+        rm -f /etc/hosts.deny
         mv /etc/hosts.deny.new /etc/hosts.deny
         systemctl restart dnsmasq.service
         exit 0
     fi
 fi
 
+rm -f /etc/hosts.deny.new
 exit 1
