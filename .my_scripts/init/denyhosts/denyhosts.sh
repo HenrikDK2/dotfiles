cdda7fe9588a4c583797a6fbf91b51960bf73f6e
HenrikDK2 <henrik265@outlook.dk>
2022-12-18 14:19:19 +0100

    Improvements to denyhosts service

--- .my_scripts/init/denyhosts/denyhosts.sh
+++ .my_scripts/init/denyhosts/denyhosts.sh
@@ -3,7 +3,16 @@
 renice -n 20 $$
 ionice -c idle -p $$
 
-wget --limit-rate=200k  -O "/etc/hosts.deny" https://hosts.ubuntu101.co.za/hosts
-systemctl restart dnsmasq.service
+wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
 
-exit 0
+if [[ $? -eq 0 ]]; then
+    wget --limit-rate=200k  -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts
+    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "100" ]; then
+        sudo rm /etc/hosts.deny
+        mv /etc/hosts.deny.new /etc/hosts.deny
+        systemctl restart dnsmasq.service
+        exit 0
+    fi
+fi
+
+exit 1
