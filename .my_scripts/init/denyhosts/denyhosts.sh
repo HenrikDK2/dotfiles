d3a7cfd0e13602503f4cb16d390a7106bb1ecbc3
HenrikDK2 <henrik265@outlook.dk>
2022-12-20 15:34:12 +0100

    Added new entry, and remove limit rate

--- .my_scripts/init/denyhosts/denyhosts.sh
+++ .my_scripts/init/denyhosts/denyhosts.sh
@@ -7,17 +7,19 @@ wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
 
 if [[ $? -eq 0 ]]; then
     # Output and combine hosts files
-    wget --limit-rate=200k -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts # Ultimate-Hosts-Blacklist
-    wget --limit-rate=200k -O - >> "/etc/hosts.deny.new" https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts # StevenBlack hosts
-    wget --limit-rate=200k -O - >> "/etc/hosts.deny.new" https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt # lightswitch05 hosts 
-   
+    wget -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts # Ultimate-Hosts-Blacklist
+    wget -O - >> "/etc/hosts.deny.new" https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts # StevenBlack hosts
+    wget -O - >> "/etc/hosts.deny.new" https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt # lightswitch05 hosts 
+    wget -O - >> "/etc/hosts.deny.new" https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt # Block Web bitcoin mining
+    wget -O - >> "/etc/hosts.deny.new" https://blocklistproject.github.io/Lists/ads.txt # Blocklist project (ads)
+
     # Remove duplicates and comments
     sed -i '/^[[:blank:]]*#/d;s/[[:blank:]]*#.*//' /etc/hosts.deny.new
     awk -i inplace '!seen[$0]++' /etc/hosts.deny.new
     sed -i "7d" /etc/hosts.deny.new
 
     # Replace current hosts.deny with new one
-    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "765000" ]; then
+    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "850000" ]; then
         rm -f /etc/hosts.deny
         mv /etc/hosts.deny.new /etc/hosts.deny
         systemctl restart dnsmasq.service
