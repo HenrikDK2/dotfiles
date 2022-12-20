979fbcba2022a8a0eee875076b3351933ff9503a
HenrikDK2 <henrik265@outlook.dk>
2022-12-20 14:46:58 +0100

    Added another hosts source, and remove comments

--- .my_scripts/init/denyhosts/denyhosts.sh
+++ .my_scripts/init/denyhosts/denyhosts.sh
@@ -7,14 +7,16 @@ wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
 
 if [[ $? -eq 0 ]]; then
     # Output and combine hosts files
-    wget --limit-rate=200k -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts
-    wget --limit-rate=200k -O - >> "/etc/hosts.deny.new" https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+    wget --limit-rate=200k -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts # Ultimate-Hosts-Blacklist
+    wget --limit-rate=200k -O - >> "/etc/hosts.deny.new" https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts # StevenBlack hosts
+    wget --limit-rate=200k -O - >> "/etc/hosts.deny.new" https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt # lightswitch05 hosts 
    
-    # Remove duplicates
+    # Remove duplicates and comments
+    sed -i '/^[[:blank:]]*#/d;s/[[:blank:]]*#.*//' /etc/hosts.deny.new
     awk -i inplace '!seen[$0]++' /etc/hosts.deny.new
 
     # Replace current hosts.deny with new one
-    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "750000" ]; then
+    if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "765000" ]; then
         rm -f /etc/hosts.deny
         mv /etc/hosts.deny.new /etc/hosts.deny
         systemctl restart dnsmasq.service
