300ee33f099adcccfdd97c3a0926205b482f60e7
HenrikDK2 <henrik265@outlook.dk>
2023-01-02 18:28:59 +0100

    Improvements to denyhosts

--- .my_scripts/init/denyhosts/denyhosts.sh
+++ .my_scripts/init/denyhosts/denyhosts.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-renice -n 20 $$
-ionice -c idle -p $$
+add_source(){
+	wget -O - >> "/etc/hosts.deny.new" $1
+}
 
 update_hosts(){
     wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
 
     if [[ $? -eq 0 ]]; then
-        # Output and combine hosts files
-        wget -O "/etc/hosts.deny.new" https://hosts.ubuntu101.co.za/hosts # Ultimate-Hosts-Blacklist
-        wget -O - >> "/etc/hosts.deny.new" https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts # StevenBlack hosts
-        wget -O - >> "/etc/hosts.deny.new" https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt # lightswitch05 hosts 
-        wget -O - >> "/etc/hosts.deny.new" https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt # Blocking Web Browser Bitcoin Mining
-        wget -O - >> "/etc/hosts.deny.new" https://blocklistproject.github.io/Lists/ads.txt # Blocklist project (ads)
-
-        # Remove duplicates and comments
-        sed -i '/^[[:blank:]]*#/d;s/[[:blank:]]*#.*//' /etc/hosts.deny.new
+        add_source https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist/master/hosts/hosts0
+       	add_source https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt
+ 		add_source https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+ 		add_source https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
+ 		add_source https://raw.githubusercontent.com/HexxiumCreations/threat-list/gh-pages/hosts.txt
+		add_source https://malware-filter.gitlab.io/malware-filter/phishing-filter-hosts.txt
+		
+        # Remove comments and duplicates
+        sed -i '/^[[:blank:]]*#/d;s/[[:blank:]]*#.*/!d/' /etc/hosts.deny.new
         awk -i inplace '!seen[$0]++' /etc/hosts.deny.new
-        sed -i "7d" /etc/hosts.deny.new
 
         # Replace current hosts.deny with new one
         if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "100" ]; then
@@ -34,6 +34,5 @@ update_hosts(){
         update_hosts
     fi
 }
-
 
 update_hosts
