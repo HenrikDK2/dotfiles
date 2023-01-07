8b5db251d372c508673899d6496a908d8fe17398
HenrikDK2 <henrik265@outlook.dk>
2023-01-07 16:20:31 +0100

    Improve denyhosts

--- .my_scripts/init/denyhosts/usr/bin/denyhosts.sh
+++ .my_scripts/init/denyhosts/usr/bin/denyhosts.sh
@@ -1,33 +1,57 @@
 #!/bin/sh
 
 add_source(){
-	wget -O - >> "/etc/hosts.deny.new" $1
+	if [ -z $2 ]; then
+		wget -O - >> "/tmp/denyhosts.txt" $1
+	else
+		wget -O - > "/tmp/denyhoststemp.txt" $1
+		sed -i '/^[ \t]*#/d;/^[[:space:]]*$/d' /tmp/denyhoststemp.txt
+		sed -i "s/^/$2 /" /tmp/denyhoststemp.txt
+		cat /tmp/denyhoststemp.txt | tee -a /tmp/denyhosts.txt
+		rm -f /tmp/denyhoststemp.txt
+	fi
 }
 
 update_hosts(){
     wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
 
     if [[ $? -eq 0 ]]; then
-        add_source https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist/master/hosts/hosts0
+	    add_source https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist/master/hosts/hosts0
        	add_source https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt
  		add_source https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
  		add_source https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
  		add_source https://raw.githubusercontent.com/HexxiumCreations/threat-list/gh-pages/hosts.txt
-		add_source https://malware-filter.gitlab.io/malware-filter/phishing-filter-hosts.txt
 		add_source https://raw.githubusercontent.com/shreyasminocha/shady-hosts/main/hosts
+		add_source https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+		add_source https://urlhaus.abuse.ch/downloads/hostfile/
+		add_source https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts
+		add_source https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser
+		add_source https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt "0.0.0.0"
+		add_source https://v.firebog.net/hosts/Prigent-Malware.txt "0.0.0.0"
+		add_source https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt "0.0.0.0"
+		add_source https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt "0.0.0.0"
+		add_source https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt "0.0.0.0" 
+		add_source https://phishing.army/download/phishing_army_blocklist_extended.txt "0.0.0.0" 
+		add_source https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt "0.0.0.0"
+		add_source https://v.firebog.net/hosts/RPiList-Malware.txt "0.0.0.0"
+		add_source https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt "0.0.0.0"
+		add_source https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts "0.0.0.0"
+		add_source https://v.firebog.net/hosts/static/w3kbl.txt "0.0.0.0"
 		
         # Remove comments and duplicates
-        sed -i '/^[[:blank:]]*#/d;s/[[:blank:]]*#.*/!d/' /etc/hosts.deny.new
-        awk -i inplace '!seen[$0]++' /etc/hosts.deny.new
+        sed -i '/^[ \t]*#/d;/^[[:space:]]*$/d' /tmp/denyhosts.txt
+		sed -i '1,7d' /tmp/denyhosts.txt
+		sed -i 's/127.0.0.1 /0.0.0.0 /g' /tmp/denyhosts.txt
+        awk -i inplace '!seen[$0]++' /tmp/denyhosts.txt
 
         # Replace current hosts.deny with new one
-        if [ "$(grep -c ^ /etc/hosts.deny.new)" -ge "100" ]; then
+        if [ "$(grep -c ^ /tmp/denyhosts.txt)" -ge "100" ]; then
             rm -f /etc/hosts.deny
-            mv /etc/hosts.deny.new /etc/hosts.deny
+            mv /tmp/denyhosts.txt /etc/hosts.deny
             systemctl restart dnsmasq.service
             exit 0
         else
-            rm -f /etc/hosts.deny.new
+            rm -f /tmp/denyhosts.txt
             exit 1
         fi
     else
