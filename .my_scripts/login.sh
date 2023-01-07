9568597d2fe2c6507e7b90781c44f146292b5284
HenrikDK2 <henrik265@outlook.dk>
2023-01-07 04:28:10 +0100

    Remove code extensions script from init folder

--- .my_scripts/login.sh
+++ .my_scripts/login.sh
@@ -13,6 +13,3 @@ for script in ~/.my_scripts/login.d/*.sh; do "$script" & done
 # Launch optimization script for priorities
 sleep 10
 sudo ~/.my_scripts/gamemode/optimize.sh
-
-# Install vscode plugins (It will not install if already installed)
-~/.my_scripts/init/code-extensions.sh
