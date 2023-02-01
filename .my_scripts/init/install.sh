c4b2c76e5c2b723bc5cff8d77c7e84b4eb9edae0
HenrikDK2 <henrik265@outlook.dk>
2023-02-01 13:19:49 +0100

    Remove flags

--- .my_scripts/init/install.sh
+++ .my_scripts/init/install.sh
@@ -31,9 +31,9 @@ fi
 
 # Use ALHP mirror if supported by the CPU (https://git.harting.dev/ALHP/ALHP.GO)
 if [ -z "$(grep -F '[core-x86-64-v3]' /etc/pacman.conf)" ] && [ -n "$(lscpu | grep 'sse4_2')" ]; then
-	yay -S alhp-keyring alhp-mirrorlist --noconfirm --needed
+	yay -Syu alhp-keyring alhp-mirrorlist
 	sudo sed -i '/\[core\]/i [core-x86-64-v3]\nInclude = /etc/pacman.d/alhp-mirrorlist\n\n[extra-x86-64-v3]\nInclude = /etc/pacman.d/alhp-mirrorlist\n\n[community-x86-64-v3]\nInclude = /etc/pacman.d/alhp-mirrorlist\n' /etc/pacman.conf
-	sudo pacman -Suy --noconfirm
+	sudo pacman -Suy
 fi
 
 # Default dconf values
