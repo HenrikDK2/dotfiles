0972dafadd403afbd858c31b033c14c31b25a22a
HenrikDK2 <henrik265@outlook.dk>
2023-05-24 16:08:45 +0200

    Small fix

--- .my_scripts/login.d/update_reminder.sh
+++ .my_scripts/login.d/update_reminder.sh
@@ -7,6 +7,6 @@ week_in_seconds=$(( 7 * 24 * 60 * 60 ))
 time_diff=$(( current_time - last_modified ))
 updates_available=$(pacman -Qu --check)
 
-if [[ -n $updates_available && $time_diff -lt $week_in_seconds ]]; then
+if [[ -n "$updates_available" && $time_diff -lt $week_in_seconds ]]; then
 	kitty sh -c 'echo -e "\033[1;33mYou have not updated in over a week!\033[0m\n"; ~/.my_scripts/update.sh;'
 fi
