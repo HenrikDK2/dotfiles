9ded1e7900c1ad421868c84f488eab73093fe2dc
HenrikDK2 <henrik265@outlook.dk>
2023-05-24 09:36:42 +0200

    Improve update script

--- .my_scripts/update.sh
+++ .my_scripts/update.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-yay -Syu --noconfirm --needed
-
-while ! [ "$(pacman -Qdtq)" = "" ]; do
-	sudo pacman -Rsunc $(pacman -Qdtq) --noconfirm
-done
-
-sudo yay -Scc --noconfirm --needed
-
 clear
-echo -e "\033[1mBeginning audit.\033[0m\n"
-~/.my_scripts/audit.sh
-read -p "Press enter to continue"
+
+# Run pacman update check
+updates_available=$(pacman -Qu --check)
+
+# Check if there are updates available
+if [ -n "$updates_available" ]; then
+	yay -Syu --noconfirm --needed
+
+	while ! [ "$(pacman -Qdtq)" = "" ]; do
+		sudo pacman -Rsunc $(pacman -Qdtq) --noconfirm
+	done
+
+	sudo yay -Scc --noconfirm --needed
+
+	# Audit
+	echo -e "\033[1mBeginning audit.\033[0m\n"
+	~/.my_scripts/audit.sh
+	read -p "Press enter to continue"
+else
+	echo "No updates available"
+fi
