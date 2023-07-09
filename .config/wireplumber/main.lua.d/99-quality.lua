197759d349001f681f64cc6235aa947d10acf0b4
HenrikDK2 <henrik265@outlook.dk>
2023-07-09 04:29:59 +0200

    Only apply properties to audio outputs

--- .config/wireplumber/main.lua.d/99-quality.lua
+++ .config/wireplumber/main.lua.d/99-quality.lua
@@ -1,10 +1,6 @@
 rule = {
   matches = {
     {
-      -- Matches all sources.
-      { "node.name", "matches", "alsa_input.*" },
-    },
-    {
       -- Matches all sinks.
       { "node.name", "matches", "alsa_output.*" },
     },
