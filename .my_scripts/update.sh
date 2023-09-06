68c543f0d44a4cce0e558db7e34124c4570fb55b
HenrikDK2 <henrik265@outlook.dk>
2023-09-06 20:53:45 +0200

    Update -git packages from the aur repo every week (Otherwise I will be compiling all day with my daily updates lol)

--- .my_scripts/update.sh
+++ .my_scripts/update.sh
@@ -8,16 +8,27 @@ clear
 updates_available=$(pacman -Qu --check)
 
 # Check if there are updates available
-if [ -n "$updates_available" ]; then
+if [ -z "$updates_available" ]; then
 	## Reduce priority of script
 	renice 20 $$
 	ionice -c 3 -p $$
 	clear
 
-	# Updating
+	# Updating normal packages
 	echo -e "\033[1mUpdating packages.\033[0m\n"
 	yay -Su --noconfirm --needed
 
+	# Update -git AUR packages (Weekly)
+	last_update_seconds=$(cat ~/.cache/git-update-last)
+	current_time_seconds=$(date +%s)
+	week_in_seconds=604800  # Number of seconds in a week
+
+	# Check if the last update timestamp exists and if it's been a week since the last update
+	if [ -z "$last_update_seconds" ] || ((current_time_seconds - last_update_seconds >= week_in_seconds)); then
+	    yay -Syu --devel --noconfirm --needed
+	    echo "$(date +%s)" > ~/.cache/git-update-last
+	fi
+	
 	# Cleanup
 	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 	echo -e "\033[1mClean up.\033[0m\n"
