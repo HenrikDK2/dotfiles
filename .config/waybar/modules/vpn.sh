d3dc3e99de09e88289b9bfed42b516be4d037b1a
HenrikDK2 <henrik265@outlook.dk>
2023-09-18 10:41:53 +0200

    Add tooltip to show mullvad or nordvpn status

--- .config/waybar/modules/vpn.sh
+++ .config/waybar/modules/vpn.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-connected(){
-	echo '{"text": "Connected", "tooltip": "VPN status: connected", "class": "connected", "percentage": 100}'
-	exit 0
+connected() {
+  tooltip="${1:-VPN status: connected}"
+  echo "{\"text\": \"Connected\", \"tooltip\": \"$tooltip\", \"class\": \"connected\", \"percentage\": 100}"
+  exit 0
 }
 
 # Check if Mullvad VPN is active based on the status
-if [[ "$(mullvad status)" == *"Connected to"* ]]; then connected; fi
+if (mullvad status | grep -q "Connected to"); then
+	connected "$(mullvad status)"
+fi
 
 # Check if NordVPN is active
-if nordvpn status | grep -q "Status: Connected"; then connected fi
+if (nordvpn status | grep -q "Status: Connected"); then
+	connected "$(nordvpn status)"
+fi
 
-# Check if the tun0 interface exists
-if [ -d /proc/sys/net/ipv4/conf/tun0 ]; then connected; fi
-
-# Check if the ppp0 interface exists
-if [ -d /proc/sys/net/ipv4/conf/ppp0 ]; then connected; fi
+# Check if the tun0 or ppp0 interface exists
+if [ -d /proc/sys/net/ipv4/conf/tun0 ] || [ -d /proc/sys/net/ipv4/conf/ppp0 ]; then 
+	connected;
+fi
 
 echo '{"text": "Disconnected", "tooltip": "VPN status: disconnected", "class" :"disconnected", "percentage" :0}'
