637725e0f96cf4901152d29fd7f46a07fff0ddc6
HenrikDK2 <henrik265@outlook.dk>
2023-10-11 23:41:06 +0200

    Remove swayidle, annoying, doesn't function as I want it to

--- .my_scripts/login.d/swayidle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-config="-C ~/.config/swaylock/config"
-
-swayidle -w \
-	timeout 300 "swaylock $config" \
-	timeout 600 'swaymsg "output * power off"' \
-		resume 'swaymsg "output * power on"' \
-	before-sleep "swaylock $config"
