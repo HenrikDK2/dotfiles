d3263f0ae907cf268f8a1efef78e4b1aea1653f5
HenrikDK2 <henrik265@outlook.dk>
2023-10-19 23:14:46 +0200

    Small fixes

--- .my_scripts/login.d/fullscreen_manager.sh
+++ .my_scripts/login.d/fullscreen_manager.sh
@@ -6,7 +6,7 @@ ionice -c 3 -p $$
 swaymsg -t subscribe -m '[ "window" ]' | while read window_json; do
     window_event=$(echo ${window_json} | jq -r '.change')
 
-    # Process only focus, close,  and fullscreen events
+    # Process only focus, close, and fullscreen events
     if [[ $window_event = "focus" || $window_event = "fullscreen_mode" || $window_event = "close" ]]; then
         window_fullscreen_status=$(echo ${window_json} | jq -r '.container.fullscreen_mode')
 
@@ -15,11 +15,10 @@ swaymsg -t subscribe -m '[ "window" ]' | while read window_json; do
         elif [[ $window_fullscreen_status = "0" ]]; then
         	if ! pgrep -x "waybar" > /dev/null; then
             	waybar &
-            	
             fi
 
             if ! pgrep -x "swaybg" > /dev/null; then
-				swaybg -i $WALLPAPER -m $WALLPAPER_MODE &
+            	swaybg -i $WALLPAPER -m $WALLPAPER_MODE &
             fi
         fi
     fi
