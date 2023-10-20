db74e62ae95a2310d3e2e9ab3ff2e4e563d44fbe
HenrikDK2 <henrik265@outlook.dk>
2023-10-20 14:38:39 +0200

    Optimize script

--- .my_scripts/login.d/fullscreen_manager.sh
+++ .my_scripts/login.d/fullscreen_manager.sh
@@ -3,15 +3,14 @@
 renice 20 $$
 ionice -c 3 -p $$
 
-swaymsg -t subscribe -m '[ "window" ]' | while read window_json; do
-    window_event=$(echo ${window_json} | jq -r '.change')
+swaymsg -t subscribe -m '[ "window" ]' | while read -r window_json; do
+    window_event=$(jq -r '.change' <<< "$window_json")
 
-    # Process only focus, close, and fullscreen events
-    if [[ $window_event = "focus" || $window_event = "fullscreen_mode" || $window_event = "close" ]]; then
-        window_fullscreen_status=$(echo ${window_json} | jq -r '.container.fullscreen_mode')
+    if [[ $window_event = @(focus|fullscreen_mode|close) ]]; then
+        window_fullscreen_status=$(jq -r '.container.fullscreen_mode' <<< "$window_json")
 
         if [[ $window_fullscreen_status = "1" ]]; then
-            killall -9 waybar;
+        	killall -9 waybar
         else
         	if ! pgrep -x "waybar" > /dev/null; then
             	waybar &
