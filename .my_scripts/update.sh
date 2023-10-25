3aad5bea687cb6eb4b21267f1a7416a7c3a88894
HenrikDK2 <henrik265@outlook.dk>
2023-10-25 17:13:53 +0200

    Don't clean cache on update

--- .my_scripts/update.sh
+++ .my_scripts/update.sh
@@ -47,8 +47,6 @@ if [ -n "$updates_available" ] || [ ! -f ~/.cache/git-update-last ]; then
 		sudo pacman -Rsunc $(pacman -Qdtq) --noconfirm
 	done
 
-	yay -Scc --noconfirm --needed
-
 	# Audit
 	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 	echo -e "\033[1mBeginning audit.\033[0m\n"
