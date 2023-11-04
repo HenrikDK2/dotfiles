b5eb625db1cacfdb05dd5b16e43dc34467d0ffb5
HenrikDK2 <henrik265@outlook.dk>
2023-11-05 00:37:46 +0100

    Change timeout of notifications

--- .my_scripts/init/system/usr/local/bin/redist.sh
+++ .my_scripts/init/system/usr/local/bin/redist.sh
@@ -3,10 +3,9 @@
 export WINEPREFIX="$STEAM_COMPAT_DATA_PATH"
 
 if [ ! -f "./redist-done" ]; then
-	notify-send "Installing dlls to new prefix"
+	notify-send -u low "Installing DLLs to new prefix"
 	winetricks -q --force vcrun2022 dotnet48 version d3dcompiler_47
 	touch "redist-done"
 fi
 
 exec "$@" &
-exit 0
