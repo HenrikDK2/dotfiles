6510ef447c9a7dc458d8b710753b717b8b1b9379
HenrikDK2 <henrik265@outlook.dk>
2023-11-05 22:36:06 +0100

    Check game folder for _redist files

--- .my_scripts/redist.sh
+++ .my_scripts/redist.sh
@@ -3,25 +3,34 @@
 # This script is useful for Heroic Games Launcher
 # It will automatically install the redists files inside _redist folder
 
-export STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.local/share/steam"
-export PROTON_PATH=/usr/share/steam/compatibilitytools.d/proton-ge-custom/proton
+STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.local/share/steam"
+PROTON_PATH=/usr/share/steam/compatibilitytools.d/proton-ge-custom/proton
+GAME_FOLDER="$PWD"
 
 if [ -z "$STEAM_COMPAT_DATA_PATH" ]; then
-	export STEAM_COMPAT_DATA_PATH=$WINEPREFIX
+	STEAM_COMPAT_DATA_PATH=$WINEPREFIX
 elif [ -z "$WINEPREFIX" ]; then
-	export WINEPREFIX=$STEAM_COMPAT_DATA_PATH
+	WINEPREFIX=$STEAM_COMPAT_DATA_PATH
 fi
 
-if [ ! -f "$WINEPREFIX/redist-done" ]; then
-	notify-send -u low "Installing DLLs to new prefix"
-
-	cd "$HOME/.my_scripts/_redist" || exit 1
+function install_dlls() {
+	cd "$1" || exit 1
 
 	for file in *.exe; do
 		if [ -f "$file" ]; then
 		    $PROTON_PATH run "$file"
 		fi
 	done
+}
+
+if [ ! -f "$WINEPREFIX/redist-done" ]; then
+	notify-send -u low "Installing DLLs to new prefix"
+	install_dlls "$HOME/.my_scripts/_redist"
+
+	# Install redists files inside game folder
+	while read redist; do
+		install_dlls "$redist"
+	done <<< "$(find "$GAME_FOLDER" -type d -iname '_Redist')"
 
 	touch "$WINEPREFIX/redist-done"
 fi
