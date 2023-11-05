ce94dc23a7d0326b75cb4ae9cca5c33fb82a9bca
HenrikDK2 <henrik265@outlook.dk>
2023-11-06 00:07:40 +0100

    Check with file hash if redist hasn't been installed

--- .my_scripts/redist.sh
+++ .my_scripts/redist.sh
@@ -13,17 +13,23 @@ elif [ -z "$WINEPREFIX" ]; then
 	WINEPREFIX=$STEAM_COMPAT_DATA_PATH
 fi
 
+REDISTS_HASH_LIST="$WINEPREFIX/.redists"
+
 function install_dlls() {
 	cd "$1" || exit 1
 
 	for file in *.exe; do
-		if [ -f "$file" ]; then
-		    $PROTON_PATH run "$file"
+		local sha256=$(sha256sum "$file" | cut -d ' ' -f 1)
+
+		# If file_hash doesn't exist in the list
+		if ! grep -q "$sha256" "$REDISTS_HASH_LIST"; then
+			$PROTON_PATH run "$file"
+			echo "$sha256" >> "$REDISTS_HASH_LIST"
 		fi
 	done
 }
 
-if [ ! -f "$WINEPREFIX/redist-done" ]; then
+if [ ! -f "$REDISTS_HASH_LIST" ]; then
 	notify-send -u low "Installing DLLs to new prefix"
 	install_dlls "$HOME/.my_scripts/_redist"
 
@@ -31,8 +37,6 @@ if [ ! -f "$WINEPREFIX/redist-done" ]; then
 	while read redist; do
 		install_dlls "$redist"
 	done <<< "$(find "$GAME_FOLDER" -type d -iname '_Redist')"
-
-	touch "$WINEPREFIX/redist-done"
 fi
 
 exec "$@" &
