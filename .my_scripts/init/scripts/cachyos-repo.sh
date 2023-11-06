3042f98704ba17442bd6fdca620ed3f5d5caf683
HenrikDK2 <henrik265@outlook.dk>
2023-11-06 01:39:43 +0100

    Fix path

--- .my_scripts/init/scripts/cachyos-repo.sh
+++ .my_scripts/init/scripts/cachyos-repo.sh
@@ -4,4 +4,4 @@ wget https://mirror.cachyos.org/cachyos-repo.tar.xz -P $HOME
 tar xvf $HOME/cachyos-repo.tar.xz
 cd $HOME/cachyos-repo
 sudo ./cachyos-repo.sh
-rm -rf cachyos-repo*
+rm -rf $HOME/*cachyos-repo*
