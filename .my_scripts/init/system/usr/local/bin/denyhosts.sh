94588546ab163dcd356880ce6a3a15f3bc7d3785
HenrikDK2 <henrik265@outlook.dk>
2023-12-26 00:24:16 +0100

    Remove many of the denyhosts sources

--- .my_scripts/init/system/usr/local/bin/denyhosts.sh
+++ .my_scripts/init/system/usr/local/bin/denyhosts.sh
@@ -17,25 +17,7 @@ update_hosts(){
 
     if [[ $? -eq 0 ]]; then
 	    add_source https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist/master/hosts/hosts0
-       	add_source https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt
- 		add_source https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
- 		add_source https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
- 		add_source https://raw.githubusercontent.com/HexxiumCreations/threat-list/gh-pages/hosts.txt
-		add_source https://raw.githubusercontent.com/shreyasminocha/shady-hosts/main/hosts
-		add_source https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
-		add_source https://urlhaus.abuse.ch/downloads/hostfile/
-		add_source https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts
-		add_source https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser
-		add_source https://v.firebog.net/hosts/Prigent-Malware.txt "0.0.0.0"
-		add_source https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt "0.0.0.0"
-		add_source https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt "0.0.0.0"
-		add_source https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt "0.0.0.0" 
-		add_source https://phishing.army/download/phishing_army_blocklist_extended.txt "0.0.0.0" 
-		add_source https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt "0.0.0.0"
-		add_source https://v.firebog.net/hosts/RPiList-Malware.txt "0.0.0.0"
-		add_source https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt "0.0.0.0"
-		add_source https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts "0.0.0.0"
-		add_source https://v.firebog.net/hosts/static/w3kbl.txt "0.0.0.0"
+       	add_source https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 		
         # Remove comments and duplicates
         sed -i '/^[ \t]*#/d;/^[[:space:]]*$/d' /tmp/denyhosts.txt
