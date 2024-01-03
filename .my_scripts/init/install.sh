451db5c0072832f00e1ac75353c55b8012532d2d
HenrikDK2 <henrik265@outlook.dk>
2024-01-03 20:52:18 +0100

    Change back to mesa from repo

--- .my_scripts/init/install.sh
+++ .my_scripts/init/install.sh
@@ -145,13 +145,11 @@ if [[ $(get_primary_gpu) == "nvidia" ]]; then
 	echo "Nvidia GPU drivers not yet implemented..."
 	read -p "Press enter to continue"
 elif [[ $(get_primary_gpu) == "amd" ]]; then
-	sudo pacman -S mesa-git lib32-mesa-git --noconfirm
-	#sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils --noconfirm
+	sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils --noconfirm
 	sudo sed -i "s/MODULES=()/MODULES=(amdgpu)/" /etc/mkinitcpio.conf
 	sudo mkinitcpio -P;
 elif [[ $(get_primary_gpu) == "intel" ]]; then
-	sudo pacman -S mesa-git lib32-mesa-git --noconfirm
-	#sudo pacman -S mesa lib32-mesa vulkan-intel lib32-vulkan-intel intel-media-driver --noconfirm
+	sudo pacman -S mesa lib32-mesa vulkan-intel lib32-vulkan-intel intel-media-driver --noconfirm
 fi
 
 # Packages
