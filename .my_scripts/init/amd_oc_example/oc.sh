1186f68b536c0fa014ac7d674ea0ef45384e3254
HenrikDK2 <henrik265@outlook.dk>
2024-01-13 23:10:25 +0100

    Update OC

--- .my_scripts/init/amd_oc_example/oc.sh
+++ .my_scripts/init/amd_oc_example/oc.sh
@@ -21,7 +21,7 @@ PP_OD_CLK_VOLTAGE="/sys/class/drm/$GPU_CARD/device/pp_od_clk_voltage"
 POWER_CAP="/sys/class/drm/$GPU_CARD/device/hwmon/$HWMON/power1_cap"
 
 # Voltage offset
-echo "vo -40" > $PP_OD_CLK_VOLTAGE
+echo "vo -20" > $PP_OD_CLK_VOLTAGE
 
 # Max core clock
 echo "s 1 2950" > $PP_OD_CLK_VOLTAGE
