8dc319de6f37ca141d9c25c6d88d46e4f552fec2
HenrikDK2 <henrik265@outlook.dk>
2024-02-04 13:04:29 +0100

    Improvements

--- .local/share/nemo/kill_services.sh
+++ .local/share/nemo/kill_services.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-if [ "$1" != "gamemode" ]; then
-	while pgrep -x "nemo" > /dev/null; do
-	    sleep 1
-	done
+if ! pgrep -x "nemo" > /dev/null; then
+	systemctl --user stop gvfs-daemon
+	systemctl --user stop gvfs-mtp-volume-monitor
+	systemctl --user stop gvfs-udisks2-volume-monitor
+	systemctl --user stop gvfs-afc-volume-monitor
+	systemctl --user stop gvfs-goa-volume-monitor
+	systemctl --user stop gvfs-gphoto2-volume-monitor
+	systemctl --user stop gvfs-metadata
 fi
 
-systemctl --user stop gvfs-daemon
-systemctl --user stop gvfs-mtp-volume-monitor
-systemctl --user stop gvfs-udisks2-volume-monitor
-systemctl --user stop gvfs-afc-volume-monitor
-systemctl --user stop gvfs-goa-volume-monitor
-systemctl --user stop gvfs-gphoto2-volume-monitor
-systemctl --user stop gvfs-metadata
