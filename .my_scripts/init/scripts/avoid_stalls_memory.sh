c6045a54b6cb3d511ef80577ddc863b1304e4e4b
HenrikDK2 <henrik265@outlook.dk>
2024-02-08 15:36:41 +0100

    Update

--- .my_scripts/init/scripts/avoid_stalls_memory.sh
+++ .my_scripts/init/scripts/avoid_stalls_memory.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Improve response times by avoidind stalls on memory allocations
+# Improve response times by avoiding stalls on memory allocations
 # A smoother experience at the cost of memory
 
 total_memory=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-total_memory_in_gb=$((total_memory / 1024000))  # corrected the calculation
+min_free_kbytes=$((total_memory * 1 / 100)) # 1% of memory
 
-if [ $total_memory_in_gb -ge 24 ]; then
-	min_free_kbytes=$((total_memory * 2 / 100)) # 2% of memory
-	
-	sudo sed -i "s/#MEM/$min_free_kbytes/" /etc/tmpfiles.d/performance.conf
-	sudo sed -i "s/#WSF/500/" /etc/tmpfiles.d/performance.conf # 5% of memory
-else
-	min_free_kbytes=$((total_memory * 1 / 100)) # 1% of memory
-	
-	sudo sed -i "s/#MEM/$min_free_kbytes/" /etc/tmpfiles.d/performance.conf
-	sudo sed -i "s/#WSF/100/" /etc/tmpfiles.d/performance.conf # 1% of memory
+if [ "$min_free_kbytes" -le 4096 ]; then
+    min_free_kbytes=4096
 fi
+
+sudo sed -i "s/#MEM/$min_free_kbytes/" /etc/tmpfiles.d/performance.conf
