1b446e8dc41978dbe71d3936436a9bf75fd8183e
HenrikDK2 <henrik265@outlook.dk>
2024-02-15 19:07:34 +0100

    Add improved redist script

--- .my_scripts/redist.sh
+++ .my_scripts/redist.sh
@@ -3,17 +3,19 @@
 # This script is useful for Heroic Games Launcher
 # It will automatically install the redists files inside _redist folder
 
-STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.local/share/steam"
-PROTON_PATH=/usr/share/steam/compatibilitytools.d/proton-ge-custom/proton
-GAME_FOLDER=$(echo "$PWD" | sed -E 's/(bin|bin64)$//I')
-
 if [ -z "$STEAM_COMPAT_DATA_PATH" ]; then
-	STEAM_COMPAT_DATA_PATH=$WINEPREFIX
+	STEAM_COMPAT_DATA_PATH="$WINEPREFIX"
 elif [ -z "$WINEPREFIX" ]; then
-	WINEPREFIX=$STEAM_COMPAT_DATA_PATH
+	WINEPREFIX="$STEAM_COMPAT_DATA_PATH"
 fi
 
+STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.local/share/steam"
+GAME_FOLDER=$(echo "$PWD" | sed -E 's/(bin|bin64)$//I')
 REDISTS_HASH_LIST="$WINEPREFIX/.redists"
+
+if [[ "$WINEDLLPATH" == "$HOME/.config/heroic/tools/wine/Wine-GE-Latest"* ]]; then
+	WINE_RUN="$HOME/.config/heroic/tools/wine/Wine-GE-Latest/bin/wine"
+fi
 
 function install_dlls() {
     local redist_dir="$1"
@@ -27,14 +29,13 @@ function install_dlls() {
         local sha256=$(sha256sum "$file" | cut -d ' ' -f 1)
 
         if ! grep -q "$sha256" "$REDISTS_HASH_LIST"; then
-            $PROTON_PATH run "$file"
+            $WINE_RUN $file
             echo "$sha256" >> "$REDISTS_HASH_LIST"
         fi
     done
 }
 
-if [ ! -f "$REDISTS_HASH_LIST" ]; then
-	notify-send -u low "Installing DLLs to new prefix"
+if [ ! -z "$WINE_RUN" ]; then
 	install_dlls "$HOME/.my_scripts/_redist"
 
 	# Install redists files inside game folder
