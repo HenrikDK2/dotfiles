c25883adf19b2f538500807ec7c1957e44bd80af
HenrikDK2 <henrik265@outlook.dk>
2024-02-18 15:46:38 +0100

    Remove alph script

--- .my_scripts/init/system/etc/pacman.d/scripts/alhp-mirrorlist-remove.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i '/^\[core-x86-64-v3\]/,/^Include = \/etc\/pacman.d\/alhp-mirrorlist$/d' /etc/pacman.conf
-sed -i '/^\[extra-x86-64-v3\]/,/^Include = \/etc\/pacman.d\/alhp-mirrorlist$/d' /etc/pacman.conf
-sed -i '/^\[multilib-x86-64-v3\]/,/^Include = \/etc\/pacman.d\/alhp-mirrorlist$/d' /etc/pacman.conf
-
-# Remove multiple newlines
-sed -i '/^$/N;/^\n$/D' /etc/pacman.conf
--- .my_scripts/init/system/etc/pacman.d/scripts/alhp-mirrorlist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Remove worldwide mirror
-sudo sed -i '/https:\/\/alhp.krautflare.de\/$repo\/os\/$arch\//d' /etc/pacman.d/alhp-mirrorlist
-
-# Resolve potential issues with signatures
-rm -rf /etc/pacman.d/gnupg/
-pacman-key --init
-pacman-key --populate
