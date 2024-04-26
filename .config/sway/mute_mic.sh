5a61dffae71ff49029bfb5c2f2e2bfbb75c99c91
HenrikDK2 <henrik265@outlook.dk>
2024-04-26 17:54:37 +0200

    Swap text

--- .config/sway/mute_mic.sh
+++ .config/sway/mute_mic.sh
@@ -8,8 +8,8 @@ mute_status=$(pactl get-source-mute "$source_name" | awk '{print $2}')
 # Toggle mute
 if [ "$mute_status" == "yes" ]; then
     pactl set-source-mute "$source_name" 0
-    notify-send --urgency=low "Microphone is muted"
+    notify-send --urgency=low "Microphone is unmuted"
 elif [ "$mute_status" == "no" ]; then
     pactl set-source-mute "$source_name" 1
-    notify-send --urgency=low "Microphone is unmuted"
+    notify-send --urgency=low "Microphone is muted"
 fi
