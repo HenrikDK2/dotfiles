623d369da5160f918d579f50a4847b0153abd428
HenrikDK2 <henrik265@outlook.dk>
2024-04-26 17:51:56 +0200

    Send notification when toggle mute mic

---
+++ .config/sway/mute_mic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source_name=$(pactl get-default-source)
+
+# Get the mute status of the specified source
+mute_status=$(pactl get-source-mute "$source_name" | awk '{print $2}')
+
+# Toggle mute
+if [ "$mute_status" == "yes" ]; then
+    pactl set-source-mute "$source_name" 0
+    notify-send --urgency=low "Microphone is muted"
+elif [ "$mute_status" == "no" ]; then
+    pactl set-source-mute "$source_name" 1
+    notify-send --urgency=low "Microphone is unmuted"
+fi
