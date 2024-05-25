55ad18eeb51cf84166f2ad3089bd884f503b745b
HenrikDK2 <henrik265@outlook.dk>
2024-05-25 13:41:18 +0200

    If no internet connection is detected, wait 10 secs and try again

--- .my_scripts/init/system/usr/local/bin/mirrors.sh
+++ .my_scripts/init/system/usr/local/bin/mirrors.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-if ! command -v rate-mirrors &> /dev/null; then
-    echo "Error: rate-mirrors command is not available." >&2
-    exit 1
-fi
+internet_loops=0
 
-if ! command -v cachyos-rate-mirrors &> /dev/null; then
-    echo "Error: cachyos-rate-mirrors command is not available." >&2
-    exit 1
-fi
+main () {
+	if ! command -v rate-mirrors &> /dev/null; then
+	    echo "Error: rate-mirrors command is not available." >&2
+	    exit 1
+	fi
 
-# Check for an internet connection
-if ! ping -c 1 google.com >/dev/null 2>&1; then
-  echo "An internet connection is required to run this script."
-  exit 1
-fi
+	if ! command -v cachyos-rate-mirrors &> /dev/null; then
+	    echo "Error: cachyos-rate-mirrors command is not available." >&2
+	    exit 1
+	fi
 
-# Update arch mirrors
-rate-mirrors --disable-comments-in-file --allow-root --protocol=https arch | tee /etc/pacman.d/mirrorlist
+	# Check for an internet connection
+	if ! ping -c 1 google.com >/dev/null 2>&1; then
+	  if $internet_loops -lt 10; then
+		sleep 10
+		internet_loops=$((internet_loops + 1))
+		main
+	  else
+	  	echo "An internet connection is required to run this script."
+	  	exit 1
+	  fi
+	fi
 
-# Update cachyos mirrors
-cachyos-rate-mirrors
+	# Update arch mirrors
+	rate-mirrors --disable-comments-in-file --allow-root --protocol=https arch | tee /etc/pacman.d/mirrorlist
+
+	# Update cachyos mirrors
+	cachyos-rate-mirrors
+}
+
+main
