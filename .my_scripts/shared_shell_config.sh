4b9d054d6161d1b785fc747f32cbfe52fbbec7dc
HenrikDK2 <henrik265@outlook.dk>
2024-05-30 12:38:28 +0200

    Disable MangoHud by default, causes issues in some games

--- .my_scripts/shared_shell_config.sh
+++ .my_scripts/shared_shell_config.sh
@@ -14,9 +14,6 @@ export AMD_VULKAN_ICD="RADV"
 export RADV_PERFTEST="nggc,sam,ngg_streamout"
 export VKD3D_CONFIG="dxr11"
 
-export MANGOHUD_DLSYM=1
-export MANGOHUD=1
-
 #export MOZ_ENABLE_WAYLAND=1 Disabled until copy/paste works again
 export MOZ_WEBRENDER=1
 
