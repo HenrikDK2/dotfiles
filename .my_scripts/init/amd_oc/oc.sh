62494be4d75e9e974824bc3f7fea547cbc7f71a8
HenrikDK2 <henrik265@outlook.dk>
2024-06-01 18:03:43 +0200

    Improvements to AMD GPU overclock service/script

--- .my_scripts/init/amd_oc/oc.sh
+++ .my_scripts/init/amd_oc/oc.sh
@@ -3,10 +3,10 @@
 # CHANGE OC VARIABLE VALUES BEFORE RUNNING SCRIPT, THIS IS JUST AN EXAMPLE FILE FOR MY CURRENT OC!
 # Some changes might be needed for different generations of AMD GPU, however this works on RDNA3.
 
-voltage_offset="-30"
-core_clock="2950"
-memory_clock="1300"
-powerlimit="327" # Watts
+VOLTAGE_OFFSET="-30"
+CORE_CLOCK="2950"
+MEMORY_CLOCK="1300"
+MAX_WATTS_POWERLIMIT="327" # Watts
 
 if [ -e /sys/class/drm/card0 ]; then
     GPU_CARD="card0"
@@ -20,26 +20,35 @@ elif [ -e /sys/class/drm/$GPU_CARD/device/hwmon/hwmon2 ]; then
     HWMON="hwmon2"
 fi
 
-PP_OD_CLK_VOLTAGE="/sys/class/drm/$GPU_CARD/device/pp_od_clk_voltage"
-POWER_CAP="/sys/class/drm/$GPU_CARD/device/hwmon/$HWMON/power1_cap"
+GPU="/sys/class/drm/$GPU_CARD/device"
+PP_OD_CLK_VOLTAGE="$GPU/pp_od_clk_voltage"
+POWER_CAP="$GPU/hwmon/$HWMON/power1_cap"
+CURRENT_MEMORY_SPEED="$GPU/hwmon/$HWMON/power1_cap"
 
 # Voltage offset
-echo "vo $voltage_offset" > $PP_OD_CLK_VOLTAGE
+echo "vo $VOLTAGE_OFFSET" > $PP_OD_CLK_VOLTAGE
 
 # Core clock
-echo "s 1 $core_clock" > $PP_OD_CLK_VOLTAGE
+echo "s 1 $CORE_CLOCK" > $PP_OD_CLK_VOLTAGE
 
 # Memory clock
-echo "m 1 $memory_clock" > $PP_OD_CLK_VOLTAGE
+echo "m 1 $MEMORY_CLOCK" > $PP_OD_CLK_VOLTAGE
 
-# Powerlimit (327W)
-#echo "$powerlimit" > $POWER_CAP
+# Powerlimit
+#echo "$(($MAX_WATTS_POWERLIMIT * 1000000))" > $POWER_CAP
 
 # Apply settings
 echo "c" > $PP_OD_CLK_VOLTAGE
 
-# Hacky fix to solve memory clock sometimes getting stock
-sleep 5
-memory_clock=$((memory_clock + 1))
-echo "m 1 $memory_clock" > $PP_OD_CLK_VOLTAGE
-echo "c" > $PP_OD_CLK_VOLTAGE
+# Fix memory stuck
+while true; do
+	if [[ "$MEMORY_SPEED" == "772Mhz" ]]; then
+		echo "m 1 $(($MEMORY_CLOCK + 1))" > $PP_OD_CLK_VOLTAGE
+		echo "c" > $PP_OD_CLK_VOLTAGE
+		sleep 5
+		echo "m 1 $new_MEMORY_CLOCK" > $PP_OD_CLK_VOLTAGE
+		echo "c" > $PP_OD_CLK_VOLTAGE
+	fi
+
+	sleep 300
+done
