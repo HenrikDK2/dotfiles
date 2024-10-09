fd6017dcbcb0141b4317df8e827937bfddbe27bc
HenrikDK2 <henrik265@outlook.dk>
2024-10-09 20:03:40 +0200

    Improve Heroic config settings comment to explain condition

--- .my_scripts/init/scripts/heroic.sh
+++ .my_scripts/init/scripts/heroic.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Config settings for Heroic Games Launcher
+# The script will copy the config settings only if the Heroic folder is empty.
+
 if [ ! -d "$HOME/.config/heroic" ]; then
 	cp -r $HOME/.my_scripts/init/heroic $HOME/.config
 	sed -i "s/#NAME/$USER/" $HOME/.config/heroic/config.json
