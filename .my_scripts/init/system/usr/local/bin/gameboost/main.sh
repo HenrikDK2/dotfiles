e81079e49e484a32b5a94353a5a0587764011a68
HenrikDK2 <henrik265@outlook.dk>
2025-01-01 11:24:54 +0100

    Optimized CPU time for GameBoost

--- .my_scripts/init/system/usr/local/bin/gameboost/main.sh
+++ .my_scripts/init/system/usr/local/bin/gameboost/main.sh
@@ -3,51 +3,39 @@
 total_mem=$(free -m | awk '/^Mem:/ {print $2}') # Get total system memory in MB
 threshold_mem=$((total_mem / 2)) # 50% of total memory
 min_ram_limit=$((threshold_mem < 2000 ? threshold_mem : 2000)) # Whichever is lower 2GB or 50% of ram
-
-gpu_info=$(lspci | grep -iE "AMD/ATI|Intel|NVIDIA")
-gpu_usage=0
-
 is_start_script_started=false
 
 # Function to check if any game-related processes are running
 is_game_running() {
     if pids=$(pgrep -f "(proton|gamescope|minecraft|Wine-GE)"); then
         renice -n -19 -p $pids >/dev/null 2>&1
-
-		# Since Proton, Gamescope or Wine-GE is running, then we know it's a game
-		# So find and adjust priority of all running .exe processes
-		if exe_pids=$(pgrep -f "\.exe"); then
-		    renice -n -19 -p $exe_pids >/dev/null 2>&1
-		fi
         
+        # Since Proton, Gamescope or Wine-GE is running, then we know it's a game
+        # So find and adjust priority of all running .exe processes
+        if exe_pids=$(pgrep -f "\.exe"); then
+            renice -n -19 -p $exe_pids >/dev/null 2>&1
+        fi
+
         return 0
     fi
-    
+
     return 1
 }
 
-is_gpu_usage_above_50() {
-	if [[ "$gpu_info" =~ NVIDIA ]]; then
-	    # NVIDIA GPU: Use nvidia-smi
-	    command -v nvidia-smi >/dev/null && usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | head -n 1)
-	else
-	    # AMD/Intel GPU: Use gpu_busy_percent
-	    [[ -f /sys/class/drm/card0/device/gpu_busy_percent ]] && usage=$(< /sys/class/drm/card0/device/gpu_busy_percent)
-	fi
-
-    # If above 50, then return 0, if not 1
-    [[ $usage -gt 50 ]]
+is_gpu_usage_above_30() {
+    usage=$( [[ -f /sys/class/drm/card0/device/gpu_busy_percent ]] && cat /sys/class/drm/card0/device/gpu_busy_percent || nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | head -n 1 )
+    [[ $usage -gt 30 ]]
 }
 
 # Check gpu and system memory activity to determine if a game is running
 check_game_activity() {
-    is_gpu_usage_above_50 || return 1
-    
+    is_gpu_usage_above_30 || return 1
+
     if pid=$(ps --no-headers -eo pid,rss | awk -v limit=$((min_ram_limit*1024)) '$2 > limit {print $1; exit}'); then
         renice -n -19 -p "$pid" >/dev/null 2>&1
         return 0
     fi
-    
+
     return 1
 }
 
@@ -64,6 +52,6 @@ while true; do
         ./usr/local/bin/gameboost/exit.sh >/dev/null 2>&1 &
         is_start_script_started=false
     fi
-    
+
     sleep 30
 done
