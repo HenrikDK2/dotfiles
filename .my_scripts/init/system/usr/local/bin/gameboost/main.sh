19a419af6c105825ed7c1b28dbadb4296342f7bd
HenrikDK2 <henrik265@outlook.dk>
2025-01-01 01:20:29 +0100

    More improvements to service and script, now if service is exited prematurely, it will revert changes

--- .my_scripts/init/system/usr/local/bin/gameboost/main.sh
+++ .my_scripts/init/system/usr/local/bin/gameboost/main.sh
@@ -10,15 +10,12 @@ is_start_script_started=false
 
 # Function to check if any game-related processes are running
 is_game_running() {
-    pids=$(pgrep -f "(proton|gamescope|minecraft)")
-
-    if [ -n "$pids" ]; then
-        # Change niceness of all matching processes to -19
-		renice -n -19 -p $pids >/dev/null 2>&1
-        return 0  # Return 0 if any game-related process is found
-    else
-        return 1  # Return 1 if no game-related processes are found
+    if pids=$(pgrep -f "(proton|gamescope|minecraft)"); then
+        renice -n -19 -p $pids 2>/dev/null
+        return 0
     fi
+    
+    return 1
 }
 
 is_gpu_usage_above_50() {
@@ -32,56 +29,40 @@ is_gpu_usage_above_50() {
 	    [[ -f /sys/class/drm/card0/device/gpu_busy_percent ]] && usage=$(< /sys/class/drm/card0/device/gpu_busy_percent)
 	fi
 
-    # Check if GPU usage is above 50%
-    if [[ $usage -gt 50 ]]; then
-        return 0
-    else
-        return 1
-    fi
+    # If above 50, then return 0, if not 1
+    [[ $usage -gt 50 ]]
 }
 
 # Check gpu and system memory activity to determine if a game is running
 check_game_activity() {
-	if ! is_gpu_usage_above_50; then
-		return 1
-	fi
-
-    pid=$(ps aux --sort=-%mem | awk 'NR>1 {if ($6/1024 > '$min_ram_limit') {print $2; exit}}')
+    is_gpu_usage_above_50 || return 1
     
-    if pid; then
-    	sudo renice -n -19 -p "$pid"
-        return 0  # Return 0 if a process is using more than min_ram_limit
-    else
-        return 1  # Return 1 if no process exceeds the min_ram_limit
+    if pid=$(ps --no-headers -eo pid,rss | awk -v limit=$((min_ram_limit*1024)) '$2 > limit {print $1; exit}'); then
+        sudo renice -n -19 -p "$pid" 2>/dev/null
+        return 0
     fi
+    
+    return 1
 }
 
 # Combine multiple checks to determine if a game is running
 is_game_detected() {
-	if is_game_running || check_game_activity; then
-		return 0
-	else
-		return 1
-	fi
+    is_game_running || check_game_activity
 }
 
 # Main loop
 while true; do
     if is_game_detected; then
-        is_game_running=true
-   	else 
-		is_game_running=false
-   	fi
-
-    if [ "$is_game_running" == "true" ] && [ "$is_start_script_started" == "false" ]; then
-        echo "Game is detected - Switching to performance mode"
-   		(./usr/local/bin/gameboost/start.sh >/dev/null 2>&1 &)
-        is_start_script_started=true
-    elif [ "$is_game_running" == "false" ] && [ "$is_start_script_started" == "true" ]; then
+        if [ "$is_start_script_started" = false ]; then
+            echo "Game is detected - Switching to performance mode"
+            ./usr/local/bin/gameboost/start.sh >/dev/null 2>&1 &
+            is_start_script_started=true
+        fi
+    elif [ "$is_start_script_started" = true ]; then
         echo "Game is no longer detected - Reverting back"
-        (./usr/local/bin/gameboost/exit.sh >/dev/null 2>&1 &)
+        ./usr/local/bin/gameboost/exit.sh >/dev/null 2>&1 &
         is_start_script_started=false
     fi
-
+    
     sleep 30
 done
