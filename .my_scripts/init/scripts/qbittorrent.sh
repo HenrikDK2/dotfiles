b2e5f4cc06679627fdea048e744a62a951ae38cb
HenrikDK2 <henrik265@outlook.dk>
2025-12-02 07:43:17 +0100

    Added qbitorrent

--- .my_scripts/init/scripts/qbittorrent.sh
+++ .my_scripts/init/scripts/qbittorrent.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# The script will copy the config settings only if the Heroic folder is empty.
-cp -rf $DIR/user/qBittorrent $HOME/.config
-sed -i "s/henrik/$USER/" $HOME/.config/qBittorrent/qBittorrent.conf
+# The script will copy the config settings only if the qBitorrent folder is empty.
+if [ ! -d $HOME/.config/qBittorrent ]; then
+	echo "PATH $DIR/user/qBittorrent"
+	cp -rf $DIR/user/qBittorrent $HOME/.config
+	sed -i "s/henrik/$USER/" $HOME/.config/qBittorrent/qBittorrent.conf
+fi
