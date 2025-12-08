bee04b3e94f2e7086b4e4552d58eef53d938973f
HenrikDK2 <henrik265@outlook.dk>
2025-12-08 15:44:51 +0100

    Check for theme before copying

--- .dotfiles/init/scripts/qbittorrent.sh
+++ .dotfiles/init/scripts/qbittorrent.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Only copies if config folders are missing
-if [ ! -d "$HOME/.config/qBittorrent" ]; then
-    cp -rf $SCRIPT_DIR/user/qBittorrent $HOME/.config
+if [ ! -f "$HOME/.config/qBittorrent/blue.qbtheme" ]; then
+    cp -rf $SCRIPT_DIR/user/qBittorrent $HOME/.config/qBittorrent
     sed -i "s|/home/henrik|$HOME|g" $HOME/.config/qBittorrent/qBittorrent.conf
 fi
 
