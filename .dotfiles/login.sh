6448eaff68eea1a5c3ba79b9bb16c2be93094974
HenrikDK2 <henrik265@outlook.dk>
2025-12-24 11:55:28 +0100

    Make hypridle go into power-saving mode, when idle, should save battery life on laptops (Hypridle is only running when gamemode is not active)

--- .dotfiles/login.sh
+++ .dotfiles/login.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Start background services in parallel
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-mako &
-nm-applet &
-hypridle &
-
 # Lock screen
 hyprlock
+
+# Start background services in parallel
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+nm-applet &
+mako &
+hypridle &
 
 # Run all login scripts in background
 for script in $HOME/.dotfiles/login.d/*.sh; do
