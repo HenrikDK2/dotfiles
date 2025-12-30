8c0b45b33c5f0ce35d499e41e246f0e353d2065c
HenrikDK2 <henrik265@outlook.dk>
2025-12-30 10:14:42 +0100

    Remove optiional hypridle hidden file

--- .dotfiles/login.sh
+++ .dotfiles/login.sh
@@ -7,7 +7,7 @@ hyprlock
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 nm-applet &
 mako &
-[ ! -f "$HOME/.config/hypr/.disable_hypridle" ] && hypridle &
+hypridle &
 
 # Run all login scripts in background
 for script in $HOME/.dotfiles/login.d/*.sh; do
