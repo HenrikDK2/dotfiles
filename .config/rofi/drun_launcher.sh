b562c71f62112f11cf4b79d89307f77ebf368bbf
HenrikDK2 <henrik265@outlook.dk>
2026-04-12 17:20:22 +0200

    Match keywords rofi drun

--- .config/rofi/drun_launcher.sh
+++ .config/rofi/drun_launcher.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 if ! pgrep -x rofi; then
-	export FLATPAK_GL_DRIVERS=mesa-git
 	XDG_DATA_DIRS="/usr/local/share:/usr/share:$HOME/.local/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share" rofi -theme ./styles/theme.rasi -show drun
 fi
