7af80a1c8e1d63f0ff45350fee7fee48fcc60362
HenrikDK2 <henrik265@outlook.dk>
2026-04-12 17:12:38 +0200

    Change auto-suggestion color for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -130,6 +130,8 @@ set -gx OBS_VKCAPTURE 1
 set -gx DOTNET_SYSTEM_GLOBALIZATION_INVARIANT false
 set -gx FREETYPE_PROPERTIES "hinting=true:hintstyle=hintslight:antialias=rgb:subpixel_rendering=rgb"
 set -gx GTK_A11Y "none"
+
+set fish_color_autosuggestion "#747e93"
 set -U fish_greeting
 
 # Add PATHS to fish
