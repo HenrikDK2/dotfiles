5ac4d09de551181a7e177ecea3f9b3977342b4fa
HenrikDK2 <henrik265@outlook.dk>
2026-05-12 23:49:20 +0200

    Improvements to layout script

--- .config/hypr/layout.sh
+++ .config/hypr/layout.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+WAYBAR_CFG_DEFAULT="$HOME/.config/waybar/config"
+
+ultrawide() {
+    hyprctl keyword general:layout master >/dev/null
+    hyprctl keyword general:gaps_out 0 >/dev/null
+    setsid waybar -c "$WAYBAR_CFG_DEFAULT" >/dev/null 2>&1 &
+}
+
+default() {
+    local WAYBAR_TMP_CFG
+    WAYBAR_TMP_CFG="$(mktemp /tmp/waybar-config.XXXXXX.json)"
+
+    jq '.exclusive = true' "$WAYBAR_CFG_DEFAULT" > "$WAYBAR_TMP_CFG"
+
+    hyprctl keyword general:layout dwindle >/dev/null
+    hyprctl keyword general:gaps_out 20 >/dev/null
+
+    pkill -x waybar 2>/dev/null
+    setsid waybar -c "$WAYBAR_TMP_CFG" >/dev/null 2>&1 &
+}
+
 dynamic_layout() {
-    local dims=$(hyprctl monitors -j 2>/dev/null | jq -r '.[0] | "\(.width) \(.height)"')
+    local dims width height
+
+    dims=$(hyprctl monitors -j 2>/dev/null | jq -r '.[0] | "\(.width) \(.height)"')
     read -r width height <<<"$dims"
 
+    pkill -x waybar 2>/dev/null
+
+    # ultrawide first
     if (( width * 10 > height * 25 )); then
-        pkill -x waybar 2>/dev/null
-        hyprctl keyword general:layout master >/dev/null
-        hyprctl keyword general:gaps_out 0 >/dev/null
-        WAYBAR_CONFIG="$WAYBAR_CFG_DEFAULT"
+        ultrawide
     else
-        pkill -x waybar 2>/dev/null
-        hyprctl keyword general:layout dwindle >/dev/null
-        hyprctl keyword general:gaps_out 20 >/dev/null
-        WAYBAR_CONFIG="$WAYBAR_CFG_EXCLUSIVE"
+        default
     fi
-    
-    setsid waybar  >/dev/null 2>&1 &
 }
 
 dynamic_layout
-
