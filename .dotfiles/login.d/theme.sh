1b7b08dad094b5f33a726ba3ade44b7f06ef36cb
HenrikDK2 <henrik265@outlook.dk>
2026-06-21 17:56:41 +0200

    Fix

--- .dotfiles/login.d/theme.sh
+++ .dotfiles/login.d/theme.sh
@@ -6,4 +6,3 @@ gsettings set org.gnome.desktop.interface icon-theme 'Nordic-Folders'
 gsettings set org.gnome.desktop.wm.preferences theme "Nordic-darker"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface cursor-theme "Sunity-cursors"
-gsettings set org.cinnamon.desktop.default-applications.terminal exec "$terminal"
