e377756daa1628330cee64de56fa6a35b3181dc1
HenrikDK2 <henrik265@outlook.dk>
2026-08-19 02:16:12 +0200

    Improve nemo context menu, and rename theme script to gsettings

--- .dotfiles/login.d/gsettings.sh
+++ .dotfiles/login.d/gsettings.sh
@@ -6,3 +6,9 @@ gsettings set org.gnome.desktop.interface icon-theme 'Nordic-Folders'
 gsettings set org.gnome.desktop.wm.preferences theme "Nordic-darker"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface cursor-theme "Sunity-cursors"
+
+# Terminal
+gsettings set org.cinnamon.desktop.default-applications.terminal exec alacritty
+
+# Nemo
+gsettings set org.nemo.preferences executable-text-activation 'display'
